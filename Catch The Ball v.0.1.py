33a6a1aea4adf60a44cfb698bbd4e9a2b78541d3
MedvedKopatych <caligula.sab@mail.ru>
2021-05-09 15:35:52 +0300

    light refactoring

--- Catch The Ball v.0.1.py
+++ Catch The Ball v.0.1.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame import mouse
 from pygame.draw import *
 from random import randint
 pygame.init()
@@ -15,6 +14,7 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
 
 def new_ball():
     '''рисует новый шарик '''
