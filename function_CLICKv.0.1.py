067a8441d3e33c71c7f268b59a3b581bcadcb2ae
MedvedKopatych <caligula.sab@mail.ru>
2021-05-10 17:24:41 +0300

    Added Scores count, works

--- function_CLICKv.0.1.py
+++ function_CLICKv.0.1.py
@@ -3,8 +3,11 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 1.8
+FPS = 1
 screen = pygame.display.set_mode((1200, 900))
+
+f_sys = pygame.font.SysFont('arial', 12)
+
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -12,34 +15,25 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 SCORES = 0
 
-font = pygame.font.SysFont('arial', 40)
-sc_text = font.render('SCORES:', 1, RED, BLACK)
-SCORES_pos = sc_text.get_rect(center=(520, 40))
-
-font_name = pygame.font.match_font('arial')
-def draw_text(surf, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, WHITE)
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surf.blit(text_surface, text_rect)
-
-
+sc_text = f_sys.render('SCORES', 1, RED, YELLOW)
+SCORES_pos = sc_text.get_rect(center=(600, 0))
 
 
 def new_ball():
     '''рисует новый шарик '''
     global x, y, r
     x = randint(100, 1100)
-    y = randint(100, 800)
+    y = randint(100, 900)
     r = randint(10, 100)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
+
+def click(event):
+    print(x, y, r)
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -58,15 +52,12 @@ while not finished:
             if (evt_x - x)**2 + (evt_y - y)**2 <= r**2:
                 SCORES += 1
                 print('SCORES:', SCORES)
+                screen.fill(BLACK)
             else:
                 print('Missed')
-        elif event.type == pygame.MOUSEBUTTONUP:
-            pygame.display.update()
-            screen.fill(BLACK)
     new_ball()
 
     screen.blit(sc_text, SCORES_pos)
-    draw_text(screen, str(SCORES), 40, 620, 18)
     pygame.display.update()
     screen.fill(BLACK)
 
